10d16d66540ec02c771a04460b441e332bdb74c3
robertobenfini <roberto.benfini@gmail.com>
2023-05-19 16:13:32 +0200

    [ADD] use strict

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
+"use strict";
+
 for (let num = 1; num <= 100; num ++){
 
     if(num % 15 === 0){
-        let num = 'FizzBuzz'
-        console.log(num)
+        let num = 'FizzBuzz';
+        console.log(num);
     }
     
     else if(num % 3 === 0){
-        let num = 'Fizz'
-        console.log(num)
+        let num = 'Fizz';
+        console.log(num);
     }
     
     else if(num % 5 === 0){
-        let num = 'Buzz'
-        console.log(num)
+        let num = 'Buzz';
+        console.log(num);
     }
 
     else{
-        console.log(num)
+        console.log(num);
     }
 }
 
